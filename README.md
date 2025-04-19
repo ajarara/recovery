102010dd9d0d908c8a49f00d3386013419b056f9
Ahmad Jarara <ajarara@fastmail.com>
2025-04-19 18:45:13 -0400

    Add README, add config.scm

---
+++ README.md
@@ -0,0 +1,29 @@
+# security-key aided guix recovery
+a convenient, easy to access place to minimize research needed during recovery scenarios (in case all computers are lost).
+
+Security keys form the basis of my digital security, they're cheap, easy to offsite, can (and should) be pin guarded. Ootb guix does not support them, but it's in the cookbook. This gist consolidates the important info so that in case I don't have easy access to info I can just consult this simple playbook to get started.
+
+There are two possible restore flows: either we are using qemu or running guix system natively. Both flows are well supported by off-the-shelf guix downloads, provided they are x64.
+- get a new computer
+- if going the qemu route, download the guix qemu image. If running guix natively, download the install disk.
+- set up the machine (either through the install dialogs, or in qemu's case skipping this step)
+- reconfigure into config.scm (editing as needed, it is geared towards qemu but can likely be adapted into a barebones config)
+- clone the self repository (where I have all guix configs, sops credentials, this is locked behind yubikey access)
+
+Once the self repository is accessed, things become much easier: restoring from backup, accessing credentials, updating deployments. From the point of view of this public repository, that is the end goal.
+
+### useful links
+- [github.com/ajarara/.emacs.d]()
+
+### useful snippets
+#### qemu flags for MacOS
+swap out vendor id for your vendor (1050 is yubico), probably omitting it altogether works too.
+
+ssh host forwarding is useful in case you want to build a completely new qemu image: you can use the stock qemu image to build a new one, then scp it out to the host to restart the 
+```
+qemu-system-x86_64 --accel hvf -cpu host \
+                       -device qemu-xhci \
+                       -usb -device usb-host,vendorid=0x1050 \
+                       -net user,hostfwd=tcp::10022-:22 -net nic \
+                       -m $MEMORY $QCOW_IMAGE
+```
---
+++ config.scm
@@ -0,0 +1,84 @@
+;; guix system build config.scm
+(use-modules
+ (gnu bootloader)
+ (gnu bootloader grub)
+ (gnu packages)
+ (gnu services base)
+ (gnu services desktop)
+ (gnu services ssh)
+ (gnu services security-token)
+ (gnu system)
+ (gnu system file-systems)
+ (gnu system keyboard)
+ (guix gexp))
+
+(define %nonguix-signing-key
+  (plain-file "nonguix-signing-key.pub"
+"(public-key 
+ (ecc 
+  (curve Ed25519)
+  (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)
+  )
+ )"))
+
+(operating-system
+  (host-name "recovery")
+  (timezone "America/New_York")
+  (locale "en_US.utf8")
+  (keyboard-layout (keyboard-layout "us"))
+  (services
+   (cons*
+    (service pcscd-service-type)
+    (udev-rules-service 'yubikey-rules
+                        (specification->package+output "libfido2")
+                        #:groups '("plugdev"))
+    (service openssh-service-type
+             (openssh-configuration
+              (allow-empty-passwords? #t)))
+    (modify-services %desktop-services
+      (guix-service-type
+       config =>
+       (guix-configuration
+        (inherit config)
+        (substitute-urls
+         (cons* "https://substitutes.nonguix.org" %default-substitute-urls))
+        (authorized-keys
+         (cons* %nonguix-signing-key
+                %default-authorized-guix-keys))))
+      )))
+  (packages
+   (append
+    (specifications->packages
+     (list
+      "git"
+      "gnupg"
+      "emacs"
+      "openssh"
+      "alacritty"
+      "parted"
+      "i3-wm"
+      "i3status"
+      ;; dmenu isn't strictly necessary (nor is i3 lol)
+      ;; but i3 and dmenu pair together well
+      "dmenu"))
+    %base-packages))
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-bootloader)
+    (targets "/dev/vda")
+    (terminal-outputs '(console))))
+  (file-systems
+   (cons*
+    (file-system
+      (mount-point "/")
+      (device "/dev/vda1")
+      (type "ext4"))
+    %base-file-systems))
+  (users
+   (cons*
+    (user-account
+     (name "ajarara")
+     (password (crypt "" "salt"))
+     (group "users")
+     (supplementary-groups '("wheel" "netdev" "audio" "video" "plugdev")))
+    %base-user-accounts)))
