8d31c843aa27b9c510594ba43b5458b9359bc1ce
Ahmad Jarara <git@ajarara.io>
2025-04-28 22:35:31 -0400

    Also add science, sops
    This makes it so I don't have to maintain an intermediate config
    privately. I'm okay with exposing this info.

--- substitutes.sh
+++ substitutes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo "Adding nonguix as a channel"
+echo "Adding nonguix, science, sops as channels"
 tee ~/.config/guix/channels.scm <<EOF
 (cons* (channel
         (name 'nonguix)
@@ -11,6 +11,24 @@ tee ~/.config/guix/channels.scm <<EOF
           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+(channel
+  (name 'guix-science)
+  (url "https://codeberg.org/guix-science/guix-science.git")
+  (introduction
+   (make-channel-introduction
+    "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+    (openpgp-fingerprint
+     "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+(channel
+        (name 'sops-guix)
+        (url "https://github.com/fishinthecalculator/sops-guix")
+        (branch "main")
+        ;; Enable signature verification:
+        (introduction
+         (make-channel-introduction
+          "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+          (openpgp-fingerprint
+           "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
        %default-channels)
 EOF
 
@@ -22,4 +40,12 @@ sudo guix archive --authorize <<EOF
   (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)
   )
  )
+EOF
+
+echo "Authorizing guix-science's substitute server"
+guix archive --authorize <<EOF
+(public-key
+ (ecc
+  (curve Ed25519)
+  (q #89FBA276A976A8DE2A69774771A92C8C879E0F24614AAAAE23119608707B3F06#)))
 EOF
